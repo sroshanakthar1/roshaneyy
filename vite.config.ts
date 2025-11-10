35dfd3cbe185a7117e5096ff53cdc6895ca9c27f
Roshan Akthar <sroshanakthar1@gmail.com>
2025-11-10 21:43:38 +0530

    path

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,8 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  base: process.env.VITE_BASE_PATH || "/roshaneyy",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
